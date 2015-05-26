195a296d2ea63adce9ec4262ad902fdff93373d5
azhar0100 <azharfamily786241@gmail.com>
2015-05-26 19:03:53 +0500

    This is an increase in documentation and some changes have been made.

--- Stats.js
+++ Stats.js
@@ -45,11 +45,26 @@ function log10( val ){
 				Package manager is a way to make the classes construct extensible.
 				This way the Classes module could be spread over several files or areas.
 				The Packages object will be containing some expected constructs.
-				It will have a 'level parameter' that can be used to optimize performance.
-				Some packages will have to access the classes objects as a whole while some
+				It will have a 'type' system to optimize performance and introduce 
+				simplicity.Some packages will have to access the classes objects as a
+				whole while some
 				of them will not do more than do a one time property manipulation.
 				Care must be taken to not introduce modules which are very small(that will cause maintnance to be hard) 
 				or modules which are very large(beats the purpose of this PackageManager).
+				
+				General Configuration of package
+				===============================
+
+					{
+						type:'One of many'
+						//The rest of the package is determined by the type of permission
+					}
+
+					###Bootstrap
+						Bootstrap type packages have the ability to start when the
+						create method is called so it is a very optimizing type of package.
+						Many light-medium weight packages can be defined as Bootstrap
+						packages.They do not have to be coupled with the iterator and run after the iterator.
 				*/
 
 				add : function add( pkg ){
